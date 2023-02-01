1b0782ed0bdc6efcf67cbe7d1655720acc7cab24
Rajat <Rpbhandwalkr@gmail.com>
2023-02-01 12:58:44 +0000

    product repository

---
+++ BulkyBook.DA/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using BulkyBook.DA.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DA.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)    
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            var objFromDb = _db.products.FirstOrDefault(x => x.Id == obj.Id);
+            if (objFromDb != null)
+            {
+
+            }
+        }
+    }
+}
